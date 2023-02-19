2c047fcc5036be67add14856f8595e52109554cd
alefernandezoficial <alefernandezoficial@gmail.com>
2023-02-19 10:53:50 -0300

    Updated

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto")
+
+/*
+
+Esta es la parte intermedia de nuestro proyecto
+En esta parte realizamos los chequeos exactos
+Por ultimo vamos a la ultima linea de saludo
+
+*/
+
+// Este va a ser el mensaje que se envia para despedir
+console.log("Adios")
