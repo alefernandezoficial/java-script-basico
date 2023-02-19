cd730c9635587bede3c5c19fa03ebdb1e650f6d4
alefernandezoficial <alefernandezoficial@gmail.com>
2023-02-19 10:55:50 -0300

    Updated

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-
 // Este es el mensaje de bienvenida
-console.log("Esta es la puerta de entrada al proyecto")
-
-/*
-
-Esta es la parte intermedia de nuestro proyecto
-En esta parte realizamos los chequeos exactos
-Por ultimo vamos a la ultima linea de saludo
-
-*/
-
-// Este va a ser el mensaje que se envia para despedir
-console.log("Adios")
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
